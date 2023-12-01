b9e5d1a434f079a129976702abb49d085fdb6829
kokokochet <kokokochetov@gmail.com>
2023-12-01 12:35:05 +0300

    day 1 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        return input.sumOf {
+            (it.find { ch -> ch.isDigit() }!! + "" + it.findLast { ch -> ch.isDigit() }).toLong()
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun decode(line: String) = line.replace("one", "one1one").replace("two", "two2two").replace("three", "three3three")
+        .replace("four", "four4four").replace("five", "five5five").replace("six", "six6six")
+        .replace("seven", "seven7seven").replace("eight", "eight8eight").replace("nine", "nine9nine")
 
-    // test if implementation meets criteria from the description, like:
+    fun part2(input: List<String>) = part1(input.map { decode(it) })
+
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142L)
+
+    val test2Input = readInput("Day01p2_test")
+    check(part2(test2Input) == (281L + 28 + 79))
 
     val input = readInput("Day01")
     part1(input).println()
